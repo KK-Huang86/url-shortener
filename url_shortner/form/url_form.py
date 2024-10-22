0cac52ccec683763e6247303791136b3a45bb663
Kk <iankk0806@gmail.com>
2024-10-22 16:29:02 +0800

    add views about is_active or not

--- url_shortner/form/url_form.py
+++ url_shortner/form/url_form.py
@@ -1,6 +1,6 @@
 from url_shortner.models import Url
 from django.forms import ModelForm
-from django.forms.widgets import URLInput
+from django.forms.widgets import URLInput,CheckboxInput
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from django import forms
@@ -10,12 +10,14 @@ from django import forms
 class UrlForm(ModelForm):
   class Meta:
     model=Url
-    fields=["original_url"]
+    fields=["original_url","is_active"]
     widgets={
-      "original_url":URLInput( attrs={ "class":"input input-bordered w-full max-w-xs text-base" ,"placeholder": "請輸入網址"})
+      "original_url":URLInput( attrs={ "class":"input input-bordered w-full max-w-xs text-base" ,"placeholder": "請輸入網址"}),
+      "is_active":CheckboxInput(attrs={ "class":"" ,}),
     }
     labels={
-       "original_url":"原始網址"
+      "original_url":"原始網址",
+      "is_active":"是否啟用短網址"
     }
 
   def clean_original_url(self):
--- url_shortner/views.py
+++ url_shortner/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.views.decorators.http import require_POST
 from .form.url_form import UrlForm
 from .models import Url
 import string
@@ -9,12 +10,12 @@ def index(request):
   form = UrlForm()
   return render(request,"index.html",{"form":form})
 
+@require_POST
 def shorten_url(request):
-    if request.method == "POST":
         form = UrlForm(request.POST)
         if form.is_valid():
             original_url = form.cleaned_data["original_url"]
-            print(f"Original URL: {original_url}") 
+            is_active = form.cleaned_data.get("is_active", True)
 
             existing_url = Url.objects.filter(original_url=original_url).first()
             if existing_url:
@@ -24,26 +25,30 @@ def shorten_url(request):
             else:
                 short_code = generate_short_code()
                 short_url = request.build_absolute_uri('/') + short_code
-                print(f"short_url: {short_url} ")
-                Url.objects.create(original_url=original_url, short_url=short_code)
+                Url.objects.create(original_url=original_url, short_url=short_code,is_active=is_active)
                 return render(request, 'index.html', {'short_url': short_url, 'form': form})
 
-    else:
-        form = UrlForm()
-    return render(request, 'index.html', {'form': form})
+        else:
+            form = UrlForm()
+        return render(request, 'index.html', {'form': form})
 
 def redirect_original_url(request, short_url):
     try:
-        original_url = Url.objects.get(short_url=short_url).original_url
-        return redirect(original_url)
+        url_instance = Url.objects.get(short_url=short_url)
+        if url_instance.is_active:
+            return redirect(url_instance.original_url)
+        else:
+            return render(request, '404.html', status=404)
     except Url.DoesNotExist:
         return render(request, '404.html', status=404)
 
 def generate_short_code():
     characters = string.ascii_letters + string.digits
     short_code=''.join(random.choice(characters) for i in range(6))
-    print(f"short_code:{short_code}")
     while Url.objects.filter(short_url=short_code).exists():
         short_code=''.join(random.choice(characters) for i in range(6))
 
     return short_code
+
+
+
