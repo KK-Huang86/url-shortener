f8de06abcfe269ae3e16e1914a9f0431c6c4b094
Kk <iankk0806@gmail.com>
2024-10-24 10:47:02 +0800

    fetch page info

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,13 +79,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- src/scripts/copy_url.js
+++ src/scripts/copy_url.js
@@ -1,15 +1,37 @@
 import Alpine from "alpinejs";
-Alpine.data("copy_url", () => ({
+Alpine.data("combinedFunction", () => ({
   init() {
-    console.log("hi"); // 在模型初始化時印出 "hi"
+    console.log("hi");
   },
   async copyToClipboard(url) {
     try {
-      await navigator.clipboard.writeText(url); // 使用 Clipboard API 複製文本
-      console.log("複製成功: " + url); // 提示成功消息
+      await navigator.clipboard.writeText(url);
+      console.log("複製成功: " + url);
     } catch (err) {
-      console.log("複製失敗: " + err); // 提示失敗消息
+      console.log("複製失敗: " + err);
     }
+  },
+
+  fetchPageInfo() {
+    const shortUrl = this.$refs.short_url.value;
+    console.log(shortUrl);
+    fetch(shortUrl)
+      .then((data) => {
+        this.pageInfo = this.parseHTML(data);
+      })
+      .then((response) => {
+        if (!response.ok) throw new Error("無法取得網站資訊");
+        // return response.text();
+      })
+      .catch((error) => {
+        console.error("Error fetching page:", error);
+      });
+  },
+  parseHTML(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    console.log(html);
+    return doc.querySelector("title").innerText;
   },
 }));
 
